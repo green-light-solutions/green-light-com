2efc2576c634caea94a48fb37814d40460f7092f
radekpodrazky <radek.podrazky@green-light.com>
2016-09-02 13:31:37 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,109 @@
+var gulp    = require('gulp');
+var $       = require('gulp-load-plugins')();
+var browser = require('browser-sync');
+var rimraf  = require('rimraf');
+var yaml    = require('js-yaml');
+var yargs   = require('yargs');
+var fs      = require('fs');
+
+// Check for --production flag
+var PRODUCTION = !!(yargs.argv.production);
+
+// Load settings from settings.yml
+var COMPATIBILITY = loadConfig().COMPATIBILITY;
+var PORT = loadConfig().PORT;
+var UNCSS_OPTIONS = loadConfig().UNCSS_OPTIONS;
+var PATHS = loadConfig().PATHS;
+
+
+function loadConfig() {
+    var ymlFile = fs.readFileSync('config.yml', 'utf8');
+    return yaml.load(ymlFile);
+}
+
+// Build the "dist" folder by running all of the below tasks
+gulp.task('build',
+    gulp.series(clean, gulp.parallel(sass, javascript, copy), cleanDevCode));
+
+// Dev task
+gulp.task('dev',
+    gulp.series(gulp.parallel(sass)));
+
+// Build the site, run the server, and watch for file changes
+gulp.task('default',
+    gulp.series('dev', server, watch));
+
+// Delete the "dist" folder
+// This happens every time a build starts
+function clean(done) {
+    rimraf(PATHS.dist, done);
+}
+
+// Copy files out of the assets folder
+// This task skips over the "js", and "scss" folders, which are parsed separately
+function copy() {
+    return gulp.src(PATHS.assets)
+        .pipe(gulp.dest(PATHS.dist));
+}
+
+// Compile Sass into CSS
+// In production, the CSS is compressed
+function sass() {
+    return gulp.src('app/assets/scss/screen.scss')
+        .pipe($.sourcemaps.init())
+        .pipe($.sass({
+            includePaths: PATHS.sass
+        }).on('error', $.sass.logError))
+        .pipe($.autoprefixer({
+            browsers: COMPATIBILITY
+        }))
+        // Comment in the pipe below to not run UnCSS in production
+        .pipe($.if(PRODUCTION, $.uncss(UNCSS_OPTIONS)))
+        .pipe($.if(PRODUCTION, $.cssnano()))
+        .pipe($.if(!PRODUCTION, $.sourcemaps.write('./')))
+        .pipe($.if(!PRODUCTION, gulp.dest('app/assets/css')))
+        .pipe($.if(PRODUCTION, gulp.dest(PATHS.dist + '/assets/css')))
+        .pipe(browser.reload({ stream: true }));
+}
+
+// Combine JavaScript into one file
+// In production, the file is minified
+function javascript() {
+    return gulp.src(PATHS.javascript)
+        .pipe($.sourcemaps.init())
+        .pipe($.concat('app.js'))
+        .pipe($.uglify()
+            .on('error', function (e) { console.log(e); })
+        )
+        .pipe(gulp.dest(PATHS.dist + '/assets/js'));
+}
+
+function cleanDevCode() {
+    return gulp.src(PATHS.cleanup)
+        .pipe($.removeCode({production: true}))
+        .pipe(gulp.dest(PATHS.dist));
+}
+
+// Start a server with BrowserSync to preview the site in
+function server(done) {
+    browser.init({
+        server: PATHS.dev, port: PORT
+    });
+    done();
+}
+
+// Reload the browser with BrowserSync
+function reload(done) {
+    browser.reload();
+    done();
+}
+
+// Watch for changes to static assets, pages, Sass, and JavaScript
+function watch() {
+    gulp.watch('app/*.html').on('change', gulp.series(browser.reload));
+    gulp.watch('app/assets/scss/**/*.scss', sass);
+    gulp.watch('app/assets/js/**/*.js').on('change', gulp.series(browser.reload));
+    gulp.watch('app/assets/img/**/*').on('change', gulp.series(browser.reload));
+}
+
+gulp.task(cleanDevCode);
