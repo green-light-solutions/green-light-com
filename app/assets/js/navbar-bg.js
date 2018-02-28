08621ec8e6cbc1fef1b956d6ab0fe1e6e830a1d2
Radek Podrázký <radek.podrazky@green-light.com>
2018-02-28 15:24:36 +0100

    Fix navbar active items

--- app/assets/js/navbar-bg.js
+++ app/assets/js/navbar-bg.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import 'waypoints/lib/noframework.waypoints';
 
 if (document.body.classList.contains('homepage')) {
   $(window)
@@ -22,4 +23,61 @@ if (document.body.classList.contains('homepage')) {
 
       }
     });
+
+  const productsWaypoint = new Waypoint({
+    element: document.getElementById('products'),
+    handler: (dir) => {
+      if (dir === 'down') {
+        $('#mainNav a[href="#products"]')
+          .parent()
+          .addClass('active');
+      } else {
+        $('#mainNav a[href="#products"]')
+          .parent()
+          .removeClass('active');
+      }
+    },
+  });
+
+  const usWaypoint = new Waypoint({
+    element: document.getElementById('us'),
+    handler: (dir) => {
+      if (dir === 'down') {
+        $('#mainNav a[href="#products"]')
+          .parent()
+          .removeClass('active');
+        $('#mainNav a[href="#us"]')
+          .parent()
+          .addClass('active');
+      } else {
+        $('#mainNav a[href="#products"]')
+          .parent()
+          .addClass('active');
+        $('#mainNav a[href="#us"]')
+          .parent()
+          .removeClass('active');
+      }
+    },
+  });
+
+  const jobsWaypoint = new Waypoint({
+    element: document.getElementById('jobs'),
+    handler: (dir) => {
+      if (dir === 'down') {
+        $('#mainNav a[href="#us"]')
+          .parent()
+          .removeClass('active');
+        $('#mainNav a[href="#jobs"]')
+          .parent()
+          .addClass('active');
+      } else {
+        $('#mainNav a[href="#us"]')
+          .parent()
+          .addClass('active');
+        $('#mainNav a[href="#jobs"]')
+          .parent()
+          .removeClass('active');
+      }
+    },
+  });
 }
