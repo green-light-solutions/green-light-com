9e3b6aa4a16fcb85ffbb530c702f12f2721d2740
Radek Podrázký <radek.podrazky@green-light.com>
2018-03-02 12:03:08 +0100

    Add prev arrow to people scrollbox

--- app/assets/js/people-slider.js
+++ app/assets/js/people-slider.js
@@ -9,4 +9,33 @@ if (document.body.classList.contains('homepage')) {
         scrollLeft: '+=300',
       }, 500);
     });
+  $('.scrollbox .scrollbox-prev')
+    .click((e) => {
+      e.preventDefault();
+      const $scrollbox = $('.scrollbox .scrollbox-inner');
+      $scrollbox.animate({
+        scrollLeft: '-=300',
+      }, 500);
+    });
+
+  let width = $('.scrollbox .scrollbox-inner .person').length * 420;
+
+  $('.scrollbox .scrollbox-inner')
+    .scroll((e) => {
+      const $target = $(e.target);
+
+      if ($target.scrollLeft() + $(window)
+          .width() === width) {
+        $('.scrollbox .scrollbox-next')
+          .fadeOut();
+      } else if ($target.scrollLeft() === 0) {
+        $('.scrollbox .scrollbox-prev')
+          .fadeOut();
+      } else {
+        $('.scrollbox .scrollbox-prev')
+          .fadeIn();
+        $('.scrollbox .scrollbox-next')
+          .fadeIn();
+      }
+    });
 }
